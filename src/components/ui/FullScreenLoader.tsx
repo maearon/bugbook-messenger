a34d5857d79c842fd14e8b608c1dee66fd95956f
maearon <manhng132@gmail.com>
2025-12-20 09:33:35 +0700

    Refactor FullScreenLoader component to use icons from lucide-react
    - Replaced the Image component with Bug, Loader2, and MessageCircle icons for a more dynamic loading experience.
    - Updated the component's export name from FullScreenLoader to Loading for clarity.
    - Cleaned up commented-out code for better readability.

--- src/components/ui/FullScreenLoader.tsx
+++ src/components/ui/FullScreenLoader.tsx
@@ -1,23 +1,29 @@
 "use client"
 
-import React from "react"
-import Image from "next/image"
+// import React from "react"
+// import Image from "next/image"
+import { Bug, Loader2, MessageCircle } from "lucide-react"
 
-export default function FullScreenLoader() {
+export function Loading() {
   return (
     <div
-      className="fixed inset-0 z-[9999] bg-white flex items-center justify-center"
+      className="fixed inset-0 z-[9999] flex items-center justify-center"
       data-auto-id="loading-screen"
     >
       <div className="flex flex-col items-center justify-center w-full transform -translate-y-20 ">
         <div className="relative w-28 h-28 animate-pulse">
-          <Image
+          {/* <Image
             src="/logo.png"
             alt="Loading"
             fill
             className="object-contain brightness-0 opacity-10"
             priority
-          />
+          /> */}
+          <div className="flex h-full w-full items-center justify-center">
+            <Bug className="size-28 text-[#22C55E]" />
+            <Loader2 className="size-28 animate-spin text-purple-600" />
+            <MessageCircle className="size-28 text-purple-600" />
+          </div>
         </div>
       </div>
     </div>
